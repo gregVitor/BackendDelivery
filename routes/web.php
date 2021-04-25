d94a4405b0cc8ea1b806d8c6fb23a0405de93202
Vitor Gregório <vitorgre23@gmail.com>
2021-04-25 14:58:07 -0300

    Criado Crud basico para login de usuario e autenticação de rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+
+    $router->post('register', 'Auth\AuthController@registerUser');
+
+    $router->post('login', 'Auth\AuthController@login');
+
+
+});
